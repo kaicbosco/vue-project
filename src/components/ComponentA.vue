e661504845ee0d873e12f96f9c5c9f1c431e9a0b
kaicbosco <kaicbosco@gmail.com>
2026-01-20 14:37:27 -0300

    feat: creating ComponentA.vue and ComponentB.vue for task 6 tests

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">Sou o Component A</div>
+</template>
+
+<style scoped>
+.box {
+  background-color: red;
+  padding: 20px;
+}
+</style>
---
+++ src/components/ComponentB.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">Sou o Component B</div>
+</template>
+
+<style scoped>
+.box {
+  background-color: blue;
+  padding: 20px;
+}
+</style>
