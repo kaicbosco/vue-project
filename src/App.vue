ad21f9d791b5b8fbb60f1e7a33c74aa2a95a304c
kaicbosco <kaicbosco@gmail.com>
2026-01-20 10:30:05 -0300

    feat: ExperimentCounter.vue component import in App.vue for experiment task

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExperimentCounter from './components/ExperimentCounter.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Experimento Tarefa Experimental (Questão 2)</h1>
+  <p>Importando Componente:</p>
+  <br />
+  <ExperimentCounter />
 </template>
 
 <style scoped></style>
