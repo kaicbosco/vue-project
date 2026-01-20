e32781c4fe90a494775dd4ae50d31fcbd0401ec0
kaicbosco <kaicbosco@gmail.com>
2026-01-20 14:38:57 -0300

    cleanup: commenting and cleaning unuseful content from App.vue in order to prepare for task 6

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 // import ExperimentCounter from './components/ExperimentCounter.vue'
-import ExperimentShopping from './components/ExperimentShopping.vue'
+// import ExperimentShopping from './components/ExperimentShopping.vue'
 </script>
 
 <template>
-  <!-- <h1>Experimento Tarefa Experimental (Questão 2)</h1>
-  <p>Importando Componente:</p>
-  <br />
-  <ExperimentCounter /> -->
-
-  <ExperimentShopping />
+  <h1>Experimentos</h1>
+  <!-- <ExperimentCounter /> -->
+  <!-- <ExperimentShopping /> -->
 </template>
 
 <style scoped></style>
