ea372a2c194f23a4ee433c40812f212c94cf581c
kaicbosco <kaicbosco@gmail.com>
2026-01-20 10:43:56 -0300

    feat: ExperimentShopping.vue component import in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import ExperimentCounter from './components/ExperimentCounter.vue'
+// import ExperimentCounter from './components/ExperimentCounter.vue'
+import ExperimentShopping from './components/ExperimentShopping.vue'
 </script>
 
 <template>
-  <h1>Experimento Tarefa Experimental (Questão 2)</h1>
+  <!-- <h1>Experimento Tarefa Experimental (Questão 2)</h1>
   <p>Importando Componente:</p>
   <br />
-  <ExperimentCounter />
+  <ExperimentCounter /> -->
+
+  <ExperimentShopping />
 </template>
 
 <style scoped></style>
