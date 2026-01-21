7e9a18c15775901a1614ca3a83e664ba76c4c2d4
kaicbosco <kaicbosco@gmail.com>
2026-01-21 12:18:35 -0300

    feat: importing ComponentA.vue and ComponentB.vue in App.vue in order to complete task 6 tests

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import ComponentA from './components/ComponentA.vue'
+import ComponentB from './components/ComponentB.vue'
 // import ExperimentCounter from './components/ExperimentCounter.vue'
 // import ExperimentShopping from './components/ExperimentShopping.vue'
 </script>
 
 <template>
   <h1>Experimentos</h1>
+  <ComponentA />
+  <ComponentB />
   <!-- <ExperimentCounter /> -->
   <!-- <ExperimentShopping /> -->
 </template>
