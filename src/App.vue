442413042cd32f1d9daf3867f7c32d8d312c670e
kaicbosco <kaicbosco@gmail.com>
2026-01-21 12:54:23 -0300

    cleanup: removing all unused codes that belong to previous tests and tasks at App.vue in order to begin the final task at a clean space

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
-import ComponentA from './components/ComponentA.vue'
-import ComponentB from './components/ComponentB.vue'
-// import ExperimentCounter from './components/ExperimentCounter.vue'
-// import ExperimentShopping from './components/ExperimentShopping.vue'
 </script>
 
 <template>
-  <h1>Experimentos</h1>
-  <ComponentA />
-  <ComponentB />
-  <!-- <ExperimentCounter /> -->
-  <!-- <ExperimentShopping /> -->
+  <h1>Hello World</h1>
 </template>
 
 <style scoped></style>
